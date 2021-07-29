524c399a83b710fe40cdd64ad90d15a649c7c5ca
Nicolas Thompson <mail@nicolasthompson.com>
2021-07-29 15:54:30 +0100

    adds validation

--- controllers/bakery.js
+++ controllers/bakery.js
@@ -1,22 +1,33 @@
+const { validationResult } = require("express-validator/check");
+
 exports.getCakes = (req, res, next) => {
   res.status(200).json({
     posts: [
       {
-        _id: '1',
-        name: 'First Cake',
-        comment: 'This is the first cake',
-        imageUrl: 'images/cake.jpg',
-        yumFactor: "5"
-      }
-    ]
+        id: "1",
+        name: "First Cake",
+        comment: "This is the first cake",
+        imageUrl: "images/cake.jpg",
+        yumFactor: "5",
+      },
+    ],
   });
 };
 
 exports.createCake = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res
+      .status(422)
+      .json({
+        message: "Validation failed, entered data is incorrect",
+        errors: errors.array(),
+      });
+  }
   const name = req.body.name;
   const comment = req.body.comment;
   res.status(201).json({
-    message: 'Cake created successfully',
-    post: { id: new Date().toISOString(), name: name, comment: comment }
+    message: "Cake created successfully",
+    post: { id: new Date().toISOString(), name: name, comment: comment },
   });
 };
--- routes/bakery.js
+++ routes/bakery.js
@@ -1,13 +1,24 @@
-const express = require('express');
+const express = require("express");
+const { body } = require("express-validator/check");
 
-const bakeryController = require('../controllers/bakery');
+const bakeryController = require("../controllers/bakery");
 
 const router = express.Router();
 
 // GET /bakery/cakes
-router.get('/cakes', bakeryController.getCakes);
+router.get("/cakes", bakeryController.getCakes);
 
 // POST /bakery/cake
-router.post('/cake', bakeryController.createCake);
+router.post(
+  "/cake",
+  [
+      body("id").isInt(),
+      body("name").notEmpty().trim().isLength({ max: 30 }),
+      body("comment").notEmpty().trim().isLength({ max: 200 }),
+      body("imageUrl").notEmpty(),
+      body("yumFactor").isInt({ min: 0, max: 5}),
+    ],
+  bakeryController.createCake
+);
 
 module.exports = router;
