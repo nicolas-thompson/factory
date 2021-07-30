44917ca5017ce78cdb36112d0882f42b514caee3
Nicolas Thompson <mail@nicolasthompson.com>
2021-07-30 20:53:06 +0100

    allows a cake to be updated

--- controllers/bakery.js
+++ controllers/bakery.js
@@ -1,3 +1,6 @@
+const fs = require("fs");
+const path = require("path");
+
 const { validationResult } = require("express-validator/check");
 
 const Cake = require("../models/cake");
@@ -42,7 +45,7 @@ exports.createCake = (req, res, next) => {
     .then((result) => {
       res.status(201).json({
         message: "Cake created successfully",
-        post: result,
+        cake: result,
       });
     })
     .catch((err) => {
@@ -70,4 +73,57 @@ exports.getCake = (req, res, next) => {
       }
       next(err);
     });
+};
+
+exports.updateCake = (req, res, next) => {
+  const cakeId = req.params.cakeId;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error("Validation failed, entered data is incorrect");
+    error.statusCode = 422;
+    throw error;
+  }
+  const name = req.body.name;
+  const comment = req.body.comment;
+  let imageUrl = req.body.image;
+  const yumFactor = req.body.yumFactor;
+  if (req.file) {
+    imageUrl = req.file.path;
+  }
+  if (!imageUrl) {
+    const error = new Error("No file picked.");
+    error.statusCode = 422;
+    throw error;
+  }
+  Cake.findById(cakeId)
+    .then((cake) => {
+      if (!cake) {
+        const error = new Error("Could not find cake.");
+        error.statusCode = 404;
+        throw error;
+      }
+      if (imageUrl !== cake.imageUrl) {
+        clearImage(cake.imageUrl);
+      }
+      cake.name = name;
+      cake.comment = comment;
+      cake.yumFactor = yumFactor;
+      return cake.save();
+    })
+    .then((result) =>
+      res
+        .status(200)
+        .json({ message: "Cake updated succesfully.", cake: result })
+    )
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+const clearImage = (filePath) => {
+  filePath = path.join(__dirname, "..", filePath);
+  fs.unlink(filePath, (err) => console.log(err));
 };
--- routes/bakery.js
+++ routes/bakery.js
@@ -20,4 +20,15 @@ router.post(
 
 router.get("/cake/:cakeId", bakeryController.getCake);
 
+router.put(
+  "/cake/:cakeId",
+  [
+    body("id").isInt(),
+    body("name").notEmpty().trim().isLength({ max: 5 }),
+    body("comment").notEmpty().trim().isLength({ max: 200 }),
+    body("yumFactor").isInt({ min: 0, max: 5 }),
+  ],
+  bakeryController.updateCake
+);
+
 module.exports = router;
